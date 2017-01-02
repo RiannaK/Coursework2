ae42cee185f0a527d83e6d678524645d29cd8f2a
Rianna <rianna.kelly7@gmail.com>
2017-01-02 11:55:45 +0000

    Moved script code to under the "__main__" check to provide fair code coverage statistics. From the below we can see that all lines of the update_boids method have been covered. Therefore I have confidence to begin refactoring the code.
    ----------- coverage: platform win32, python 3.5.2-final-0 -----------
    Name                                        Stmts   Miss  Cover   Missing
    -------------------------------------------------------------------------
    badboids\__init__.py                            0      0   100%
    badboids\badboids.py                           38     12    68%   40-41, 48-59
    badboids\test\__init__.py                       0      0   100%
    badboids\test\test_dummy.py                     2      0   100%
    badboids\test\test_regression_badboids.py      33     15    55%   10-16, 21-30, 51
    -------------------------------------------------------------------------
    TOTAL                                          73     27    63%
    
    ========================== 2 passed in 3.98 seconds ===========================

--- badboids/badboids.py
+++ badboids/badboids.py
@@ -3,17 +3,11 @@ A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid
 for use as an exercise on refactoring.
 """
 
+# Deliberately terrible code for teaching purposes
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Deliberately terrible code for teaching purposes
-
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
 	xs,ys,xvs,yvs=boids
@@ -42,17 +36,24 @@ def update_boids(boids):
 		ys[i]=ys[i]+yvs[i]
 
 
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
-
 def animate(frame):
    update_boids(boids)
    scatter.set_offsets(list(zip(boids[0],boids[1])))
 
 
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+
 
 if __name__ == "__main__":
-    plt.show()
+
+	boids_x = [random.uniform(-450, 50.0) for x in range(50)]
+	boids_y = [random.uniform(300.0, 600.0) for x in range(50)]
+	boid_x_velocities = [random.uniform(0, 10.0) for x in range(50)]
+	boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(50)]
+	boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+
+	figure = plt.figure()
+	axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+	scatter = axes.scatter(boids[0], boids[1])
+
+	anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
+	plt.show()
