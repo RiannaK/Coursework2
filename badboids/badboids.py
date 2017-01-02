844e02246dc149c5198686abdb2b0ec96cd5d71b
Rianna <rianna.kelly7@gmail.com>
2017-01-02 17:36:56 +0000

    Added Simulator class to badboids.py. This wraps the bulk of the logic associated with running the animation.

--- badboids/badboids.py
+++ badboids/badboids.py
@@ -3,7 +3,6 @@ A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid
 for use as an exercise on refactoring.
 """
 
-# Deliberately terrible code for teaching purposes
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
@@ -72,50 +71,64 @@ class BoidsBuilder:
         pass
 
 
-def update_boids(boids):
-    formation_flying_distance = 10000
-    formation_flying_strength = 0.125
-    alert_distance = 100
-    move_to_middle_strength = 0.01
-    delta_t = 1
+class Simulator:
+    def __init__(self, boids):
 
-    x_positions, y_positions, x_velocities, y_velocities = boids.x_positions, boids.y_positions, boids.x_velocities, boids.y_velocities
-    num_boids = boids.num_boids
-
-    for i in range(num_boids):
-        for j in range(num_boids):
-            # Fly towards the middle
-            x_position_difference = x_positions[j] - x_positions[i]
-            y_position_difference = y_positions[j] - y_positions[i]
-
-            x_velocities[i] += x_position_difference * move_to_middle_strength / num_boids
-            y_velocities[i] += y_position_difference * move_to_middle_strength / num_boids
-
-            # Fly away from nearby boids
-            if x_position_difference ** 2 + y_position_difference ** 2 < alert_distance:
-                x_velocities[i] -= x_position_difference
-                y_velocities[i] -= y_position_difference
-
-    for i in range(num_boids):
-        for j in range(num_boids):
-            # Try to match speed with nearby boids
-            x_position_difference = x_positions[j] - x_positions[i]
-            y_position_difference = y_positions[j] - y_positions[i]
-            x_velocity_difference = x_velocities[j] - x_velocities[i]
-            y_velocity_difference = y_velocities[j] - y_velocities[i]
-
-            if x_position_difference ** 2 + y_position_difference ** 2 < formation_flying_distance:
-                x_velocities[i] += x_velocity_difference * formation_flying_strength / num_boids
-                y_velocities[i] += y_velocity_difference * formation_flying_strength / num_boids
-
-    # Move according to velocities
-    x_positions += x_velocities * delta_t
-    y_positions += y_velocities * delta_t
+        self.boids = boids
+        self.figure = plt.figure()
+        axes = plt.axes(xlim=axis_limits, ylim=axis_limits)
+        self.scatter = axes.scatter(boids.x_positions, boids.y_positions)
 
 
-def animate(frame):
-    update_boids(boids)
-    scatter.set_offsets(list(zip(boids.x_positions, boids.y_positions)))
+    def update_boids(self, boids):
+        formation_flying_distance = 10000
+        formation_flying_strength = 0.125
+        alert_distance = 100
+        move_to_middle_strength = 0.01
+        delta_t = 1
+
+        x_positions, y_positions, x_velocities, y_velocities = boids.x_positions, boids.y_positions, boids.x_velocities, boids.y_velocities
+        num_boids = boids.num_boids
+
+        for i in range(num_boids):
+            for j in range(num_boids):
+                # Fly towards the middle
+                x_position_difference = x_positions[j] - x_positions[i]
+                y_position_difference = y_positions[j] - y_positions[i]
+
+                x_velocities[i] += x_position_difference * move_to_middle_strength / num_boids
+                y_velocities[i] += y_position_difference * move_to_middle_strength / num_boids
+
+                # Fly away from nearby boids
+                if x_position_difference ** 2 + y_position_difference ** 2 < alert_distance:
+                    x_velocities[i] -= x_position_difference
+                    y_velocities[i] -= y_position_difference
+
+        for i in range(num_boids):
+            for j in range(num_boids):
+                # Try to match speed with nearby boids
+                x_position_difference = x_positions[j] - x_positions[i]
+                y_position_difference = y_positions[j] - y_positions[i]
+                x_velocity_difference = x_velocities[j] - x_velocities[i]
+                y_velocity_difference = y_velocities[j] - y_velocities[i]
+
+                if x_position_difference ** 2 + y_position_difference ** 2 < formation_flying_distance:
+                    x_velocities[i] += x_velocity_difference * formation_flying_strength / num_boids
+                    y_velocities[i] += y_velocity_difference * formation_flying_strength / num_boids
+
+        # Move according to velocities
+        x_positions += x_velocities * delta_t
+        y_positions += y_velocities * delta_t
+
+
+    def animate(self, frame):
+        self.update_boids(boids)
+        self.scatter.set_offsets(list(zip(boids.x_positions, boids.y_positions)))
+
+    def run_simulation(self):
+        anim = animation.FuncAnimation(self.figure, self.animate, frames=50, interval=50)
+        plt.show()
+
 
 
 if __name__ == "__main__":
@@ -134,9 +147,7 @@ if __name__ == "__main__":
     builder.set_y_velocity_limits(y_velocity_limits)
     boids = builder.finish()
 
-    figure = plt.figure()
-    axes = plt.axes(xlim=axis_limits, ylim=axis_limits)
-    scatter = axes.scatter(boids.x_positions, boids.y_positions)
+    simulator = Simulator(boids) # todo add boids, simulation_parameters
+    simulator.run_simulation()
 
-    anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
-    plt.show()
+
