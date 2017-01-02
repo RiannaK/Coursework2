80709a115a088c1db98e2b242b9d3bbace86507d
Rianna <rianna.kelly7@gmail.com>
2017-01-02 12:24:52 +0000

    Code smell: repeated for loops. Removed repeated for loops.

--- badboids/badboids.py
+++ badboids/badboids.py
@@ -11,25 +11,25 @@ import random
 
 def update_boids(boids):
     xs, ys, xvs, yvs = boids
-    # Fly towards the middle
     for i in range(len(xs)):
         for j in range(len(xs)):
+            # Fly towards the middle
             xvs[i] += (xs[j] - xs[i]) * 0.01 / len(xs)
-    for i in range(len(xs)):
-        for j in range(len(xs)):
             yvs[i] += (ys[j] - ys[i]) * 0.01 / len(xs)
-    # Fly away from nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
+
+            # Fly away from nearby boids
             if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
                 xvs[i] = xvs[i] + (xs[i] - xs[j])
                 yvs[i] = yvs[i] + (ys[i] - ys[j])
-    # Try to match speed with nearby boids
+
     for i in range(len(xs)):
         for j in range(len(xs)):
+
+            # Try to match speed with nearby boids
             if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
                 xvs[i] += (xvs[j] - xvs[i]) * 0.125 / len(xs)
                 yvs[i] += (yvs[j] - yvs[i]) * 0.125 / len(xs)
+
     # Move according to velocities
     for i in range(len(xs)):
         xs[i] += xvs[i]
